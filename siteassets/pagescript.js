eff4a1ca02d93e828d881bc63a4ecd458ca4474d
Sophia <TheGiraffe@users.noreply.github.com>
2020-10-06 05:28:12 -1000

    see if the navbar changing data works in browser rather than on computer

--- siteassets/pagescript.js
+++ siteassets/pagescript.js
@@ -1,14 +1,18 @@
 function requestCommitDate(pagename){
-  const xhrequest = new XMLHttpRequest();
+  const githubxhrequest = new XMLHttpRequest();
   const url = 'https://api.github.com/repos/TheGiraffe/Mauipedia/commits?path=contribute/'+pagename+'&page=1&per_page=1';
-  xhrequest.open('GET',url,true);
-  xhrequest.onload = function(){
+  githubxhrequest.open('GET',url,true);
+  githubxhrequest.onload = function(){
     const getcommitDate = JSON.parse(this.response);
     var commitDate = getcommitDate[0].commit.committer.date;
     console.log(commitDate);
     var cleanupDate = new Date(commitDate).toDateString();
     $("#lastUpdated").html(cleanupDate);
   }
-  xhrequest.send();
+  githubxhrequest.send();
   /*curl -s "https://api.github.com/repos/TheGiraffe/Mauipedia/commits?path=contribute/defaultpagetemplate.html&page=1&per_page=1"|\jq -r '.[0].commit.committer.date' */
+}
+function sideBarnavBar(){
+  $("#nav").load("../siteassets/navbar.html");
+  $("#sidebar").html("this is the sidebar");
 }
