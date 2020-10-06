993d1b130fbd28fddaf46da3e66ffa44821c4cff
Sophia <TheGiraffe@users.noreply.github.com>
2020-10-06 06:57:03 -1000

    last updated now includes time as well

--- siteassets/pagescript.js
+++ siteassets/pagescript.js
@@ -7,7 +7,8 @@ function requestCommitDate(pagename){
     var commitDate = getcommitDate[0].commit.committer.date;
     console.log(commitDate);
     var cleanupDate = new Date(commitDate).toDateString();
-    $("#lastUpdated").html(cleanupDate);
+    var cleanupTime = new Date(commitDate).toTimeString();
+    $("#lastUpdated").html(cleanupDate + " " + cleanupTime);
   }
   githubxhrequest.send();
   /*curl -s "https://api.github.com/repos/TheGiraffe/Mauipedia/commits?path=contribute/defaultpagetemplate.html&page=1&per_page=1"|\jq -r '.[0].commit.committer.date' */
